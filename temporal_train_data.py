a8ab69b949af037e4ee31bd03fb1b353f6c41f53
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-12-02 18:54:25 +0000

    update

--- temporal_train_data.py
+++ temporal_train_data.py
@@ -129,9 +129,9 @@ class DataSet():
 
         while 1:
             idx += 1
-            print("Generator yielding batch No.%d" % idx)
+            print("\nGenerator yielding batch No.%d\n" % idx)
             if train_test == 'test':
-                print("Validating for job: %s" % name_str)
+                print("\nValidating for job: %s\n" % name_str)
             X, y = [], []
 
             # Generate batch_size samples.
@@ -208,7 +208,7 @@ class DataSet():
             # horizontal components
             img = None # reset to be safe
             img = cv2.imread(opt_flow_dir_x + '/frame' + "%06d" % i_frame + '.jpg', 0)
-            #print(opt_flow_dir_x + '/frame' + "%06d" % i_frame + '.jpg')
+            print(opt_flow_dir_x + '/frame' + "%06d" % i_frame + '.jpg')
             img = np.array(img)
             img = np.swapaxes(img, 0, 1)
             # mean substraction 
@@ -233,7 +233,7 @@ class DataSet():
             # reset to be safe
             img2 = None
             img2 = cv2.imread(opt_flow_dir_y + '/frame' + "%06d"%(i_frame) + '.jpg', 0)
-            #print(opt_flow_dir_y + '/frame' + "%06d" % i_frame + '.jpg')
+            print(opt_flow_dir_y + '/frame' + "%06d" % i_frame + '.jpg')
             # mean substraction 
             img2 = np.array(img2)
             img2 = np.swapaxes(img2, 0, 1)
